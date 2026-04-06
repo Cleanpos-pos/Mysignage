4c14cc9541998ad235de27437116ec56fe271c5a
Cleanpos-pos <info@posso.uk>
2026-04-06 06:25:03 +0100

    Revert "Add restaurant & takeaway directory from easyFood"
    This reverts commit 421f340525346ee1f08225c1036310590027cf54.

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,12 +121,6 @@ export default function RootLayout({
               >
                 Categories
               </Link>
-              <Link
-                href="/restaurants"
-                className="text-muted hover:text-foreground transition-colors"
-              >
-                Restaurants
-              </Link>
             </div>
           </nav>
         </header>
